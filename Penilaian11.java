d7767801b14e19b3c190f92b6bbe03182b1a0147
gendis kumala ariga <gendiskumalariga@gmail.com>
2025-11-13 18:06:04 +0700

    jobsheet8

---
+++ Penilaian11.java
@@ -0,0 +1,27 @@
+
+    import java.util.Scanner;
+
+public class Penilaian11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        int nilai;
+        float totalNilai,rataNilai;
+        
+    while (i <= 6) {
+    System.out.println("Kelompok " + i);
+
+    totalNilai = 0;
+    for (int j = 1; j <= 5; j++) {
+        System.out.print("Nilai dari kelompok penilai " + j + ": ");
+        nilai = sc.nextInt();
+        totalNilai += nilai;
+    }
+    rataNilai = totalNilai / 5;
+    System.out.println("kelompok:" +i+  "Nilai rata rata" +"=" + rataNilai);
+    i++;
+   
+}
+    }
+}
+
