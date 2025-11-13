af705917447a12181b18eab907db9ac5476707f5
gendis kumala ariga <gendiskumalariga@gmail.com>
2025-11-13 15:22:20 +0700

    jobsheet8

---
+++ Square11.java
@@ -0,0 +1,18 @@
+package jobsheet8;
+import java.util.Scanner;
+public class Square11 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System .in);
+    int n =0;
+    System.out.println(" masukkan nilai n =");
+    n = sc .nextInt();
+    for(int iOuter =1; iOuter <= n; iOuter++){
+    for( int i=1; i<=n; i++){
+        System.out.print("*");
+    }
+    System.out.println();
+}   
+}
+}
+
+
